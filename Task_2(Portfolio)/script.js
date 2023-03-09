2196faa1f90da217d80b40297a9d73a7314cf834
Shivam Kumar <109784057+Shivam-Kumar12@users.noreply.github.com>
2023-03-09 09:16:41 -0800

    Add files via upload

---
+++ Task_2(Portfolio)/script.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Programmer", "Developer", "Learner"],
+        typeSpeed: 100,
+        backSpeed: 6,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Programmer", "Developer", "Learner"],
+        typeSpeed: 100,
+        backSpeed: 6,
+        loop: true
+    });
+
+// IN this Section ,Here, connect the contact form through Google Sheet
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbzuzCexsj3yIaMJNTymKE0hp8-cYkcmNKxA0LHafQG6blrlGDBESiwDUwMuqGGKSgCfEQ/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg=document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    // .then(response => console.log('Success!', response))
+    .then(response => {
+        msg.innerHTML="Message Sent Successfully"
+        setTimeout(function(){
+            msg.innerHTML=""
+        },5000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
+});
